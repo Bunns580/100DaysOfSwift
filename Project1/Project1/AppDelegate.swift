5e4c3df4c273250a32b5f69756399bbd0ecb65ab
Paul Bunnell <bunnellrpaul@gmail.com>
2020-09-22 09:58:46 -0600

    Day 18
    Finished review of the project.

--- Project1/Project1/AppDelegate.swift
+++ Project1/Project1/AppDelegate.swift
@@ -32,6 +32,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    
+    
+    // Day 18 finished review
 
 }
 
