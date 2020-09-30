16289c8b23e48caf7b3db9e95ff6c5585e38bb85
Paul Bunnell <bunnellrpaul@gmail.com>
2020-09-30 08:47:21 -0600

    Day 23
    Completed Review for day 23.

--- Project 3/Project1/ViewController.swift
+++ Project 3/Project1/ViewController.swift
@@ -45,5 +45,6 @@ class ViewController: UITableViewController {
             navigationController?.pushViewController(vc, animated: true)
         }
     }
+    //MARK: Completed day 23 review
     
 }
