426f9167e4d623d8db50a9cae697ca35aa390eb8
Paul Bunnell <bunnellrpaul@gmail.com>
2020-10-23 08:37:52 -0600

    Day 33
    Additional day 33.

--- Project7-2/Project7-2/Petition.swift
+++ Project7-2/Project7-2/Petition.swift
@@ -1,8 +1,14 @@
 //
 //  Petition.swift
-//  Project7-2
+//  Project7
 //
 //  Created by Paul Bunnell on 10/23/20.
 //
 
 import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
