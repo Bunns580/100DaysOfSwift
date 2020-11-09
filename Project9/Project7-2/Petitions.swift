7e30d5b2724b55f859caf3be6043ba2c0d38843e
Paul Bunnell <bunnellrpaul@gmail.com>
2020-11-09 10:12:06 -0700

    Day 39
    Completion of day 39.

---
+++ Project9/Project7-2/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Project7
+//  Project7-2
+//
+//  Created by Paul Bunnell on 10/23/20.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
